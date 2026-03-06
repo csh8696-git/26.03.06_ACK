10ef8d84bd30593d8bd1a0f91118c93fa370f9a2
csh8696-git <csh8696@gmail.com>
2026-03-06 18:56:43 +0900

    soltuion1_79
    카펫

---
+++ solution1.cpp
@@ -0,0 +1,24 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int brown, int yellow) {
+	vector<int> answer;
+	int x, y;
+	int total = brown + yellow;
+	for (y = 3; y <= total / 2; y++)
+	{
+		if (total % y == 0)
+		{
+			x = total / y;
+		}
+		if ((x - 2) * (y - 2) == yellow)
+		{
+			answer.push_back(x);
+			answer.push_back(y);
+			break;
+		}
+	}
+	return answer;
+}
